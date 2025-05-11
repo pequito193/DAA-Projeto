dd25e2e106a081d7c23e8cea8b35cd37de2404ee
vitorspequito <vitorspequito@gmail.com>
2025-05-11 14:59:37 +0100

    fix: algorithm

--- DAA.py
+++ DAA.py
@@ -1,81 +1,132 @@
-import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
 class SeamCarving:
-    def __init__(self, image):
-        self.image = image.astype(np.int32)
-        self.H, self.W, _ = self.image.shape
+    # CONSTRUTOR
+    def __init__(self, image: np.ndarray):
+        self.image = image
+        self.height, self.width, _ = self.image.shape
         self.energy_map = self._calculate_energy()
 
+    # 3.2.1
     def _calculate_energy(self):
-        energy = np.zeros((self.H, self.W))
-        for y in range(1, self.H-1):
-            for x in range(1, self.W-1):
-                dx = np.sum((self.image[y, x+1] - self.image[y, x-1])**2)
-                dy = np.sum((self.image[y+1, x] - self.image[y-1, x])**2)
+        # Primeiro inicializamos a matriz de energia com zeros
+        energy = np.zeros((self.height, self.width))
+
+        for y in range(1, self.height - 1):
+            for x in range(1, self.width - 1):
+                # Calcula o gradiente em x
+                dx_r = int(self.image[y, x + 1, 0]) - int(self.image[y, x - 1, 0])
+                dx_g = int(self.image[y, x + 1, 1]) - int(self.image[y, x - 1, 1])
+                dx_b = int(self.image[y, x + 1, 2]) - int(self.image[y, x - 1, 2])
+                dx = dx_r**2 + dx_g**2 + dx_b**2
+
+                # Calcula o gradiente em y
+                dy_r = int(self.image[y + 1, x, 0]) - int(self.image[y - 1, x, 0])
+                dy_g = int(self.image[y + 1, x, 1]) - int(self.image[y - 1, x, 1])
+                dy_b = int(self.image[y + 1, x, 2]) - int(self.image[y - 1, x, 2])
+                dy = dy_r**2 + dy_g**2 + dy_b**2
+
+                # Calcula a energia
                 energy[y, x] = np.sqrt(dx + dy)
+
         return energy
 
+    # 3.3.2
     def find_vertical_steam(self):
-        H, W = self.H, self.W
-        energy = self.energy_map
-        dist_to = np.full((H, W), np.inf)
-        edge_to = np.zeros((H, W), dtype=np.int32)
+        # Matriz para armazenar o custo acumulado
+        dist_to = np.full((self.height, self.width), np.inf)
 
-        dist_to[0] = energy[0]
-        for y in range(1, H):
-            for x in range(W):
+        # Matriz para armazenar o caminho (pixel anterior)
+        edge_to = np.zeros((self.height, self.width), dtype=np.int32)
+
+        # Inicializamos a primeira linha com os valores de energia
+        dist_to[0] = self.energy_map[0]
+
+        # Preenchemos as linhas seguintes
+        for y in range(1, self.height):
+            for x in range(self.width):
+                # Pixel da esquerda e direita, bem como o próprio pixel
                 for dx in [-1, 0, 1]:
                     prev_x = x + dx
-                    if 0 <= prev_x < W:
-                        if dist_to[y, x] > dist_to[y-1, prev_x] + energy[y, x]:
-                            dist_to[y, x] = dist_to[y-1, prev_x] + energy[y, x]
+                    if 0 <= prev_x < self.width:
+                        if dist_to[y, x] > dist_to[y - 1, prev_x] + self.energy_map[y, x]:
+                            dist_to[y, x] = dist_to[y - 1, prev_x] + self.energy_map[y, x]
                             edge_to[y, x] = prev_x
 
+        # Encontra o pixel final com menor custo na última linha
         min_end = np.argmin(dist_to[-1])
         seam = []
-        for y in range(H-1, -1, -1):
+
+        # Reconstrói o caminho de menor custo
+        for y in range(self.height - 1, -1, -1):
             seam.append((y, min_end))
             min_end = edge_to[y, min_end]
+
+        # Invertemos a ordem para obter o caminho na ordem correta
         seam.reverse()
         return seam
 
+    # 3.4.1
     def remove_vertical_steam(self, seam):
-        new_image = np.zeros((self.H, self.W - 1, 3), dtype=np.int32)
+        H, W, _ = self.image.shape
+        new_image = np.zeros((H, W - 1, 3), dtype=self.image.dtype)
+
         for y, x in seam:
+            # Dá delete ao pixel da seam na linha y
             new_image[y, :, :] = np.delete(self.image[y, :, :], x, axis=0)
+
         self.image = new_image
-        self.H, self.W = new_image.shape[:2]
+        self.height, self.width = new_image.shape[:2]
         self.energy_map = self._calculate_energy()
 
     def picture(self):
         return self.image.astype(np.uint8)
-
-
-def resize_image_seam_carving(image, scale_w=None, scale_h=None):
+    
+# Para testar o energy map
+def get_energy_map(image):
     sc = SeamCarving(np.array(image))
-    if scale_w:
-        new_width = int(sc.W * scale_w)
-        while sc.W > new_width:
+    energy_map = sc.energy_map
+    return energy_map
+
+# 4.1
+def resize_image(image, width_factor=None, height_factor=None):
+    sc = SeamCarving(np.array(image))
+    
+    # Para a largura:
+    if width_factor:
+        new_width = int(sc.width * width_factor)
+        while sc.width > new_width:
             seam = sc.find_vertical_steam()
             sc.remove_vertical_steam(seam)
-    # Implementar caso scale_h para costura horizontal (transpor a imagem)
-    return Image.fromarray(sc.picture())
+    
+    # Para a altura:
+    if height_factor:
+        new_height = int(sc.height * height_factor)
+        sc.image = np.transpose(sc.image, (1, 0, 2))
+        sc.height, sc.width = sc.width, sc.height
+        sc.energy_map = sc._calculate_energy()
+        while sc.width > new_height:
+            seam = sc.find_vertical_steam()
+            sc.remove_vertical_steam(seam)
+        sc.image = np.transpose(sc.image, (1, 0, 2))
+        sc.height, sc.width = sc.width, sc.height
+        sc.energy_map = sc._calculate_energy()
+    
+    return sc.picture()
 
-from PIL import Image
 
-img1 = Image.open("img-broadway_tower.jpg")
-resized_img1 = resize_image_seam_carving(img1, scale_w=0.7)
-plt.imshow(resized_img1)
-plt.title("Reduzida para 70% da largura original")
-plt.axis("off")
-plt.show()
+# E agora testamos tudo
+imagem_1 = mpimg.imread("img-broadway_tower.jpg")
+imagem_2 = mpimg.imread("img-brent-cox-unsplash.jpg")
 
-img2 = Image.open("img-brent-cox-unsplash.jpg")
-resized_img2 = resize_image_seam_carving(img2.transpose(Image.TRANSPOSE), scale_w=0.6)
-resized_img2 = resized_img2.transpose(Image.TRANSPOSE)
-plt.imshow(resized_img2)
-plt.title("Reduzida para 60% da altura original")
-plt.axis("off")
+energyMap_1 = get_energy_map(imagem_1)
+energyMap_2 = get_energy_map(imagem_2)
+
+nova_imagem_1 = resize_image(imagem_1, width_factor=0.7)
+#nova_imagem_2 = resize_image(imagem_2, height_factor=0.6)
+
+# O resultado:
+plt.imshow(nova_imagem_1)
 plt.show()
